7debfc85d936e865c74783c25519b72b46e787db
Joe Averbukh <joeaverbukh@gmail.com>
2024-04-04 19:11:42 -0400

    Add live reacts

---
+++ src/components/Login.js
@@ -0,0 +1,150 @@
+import React, { useState } from "react"
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { init } from "@instantdb/react"
+
+const db = init({ appId: "d7e379b9-9744-4ba1-a7d4-4b022080338d" })
+
+// Styles
+const inputStyle = "box-border block w-full rounded-md border-0 px-4 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-4"
+const authContainerStyle = "box-border rounded-lg overflow-hidden bg-white outline outline-1 -outline-offset-1 outline-gray-900/10 max-w-md mx-auto flex flex-col p-6";
+const authHeaderStyle = "text-black text-2xl font-semibold tracking-tight mt-8";
+const authSubheaderStyle = "text-black text-base leading-relaxed mt-2";
+
+// Helpers
+// ----------------------
+function AuthButton({ label }) {
+  return (
+    <button className="appearance-none border-0 font-bold rounded-md cursor-pointer px-3.5 py-2.5 text-sm bg-violet-500 hover:bg-violet-400 text-white shadow-sm mt-4 w-full">
+      {label}
+    </button>
+  )
+}
+
+function Background() {
+  return (
+    <img className="absolute inset-0 w-full h-full -z-10" alt="" src="data:image/svg+xml,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%3F%3E%0A%3Csvg%20width%3D%221000px%22%20height%3D%221000px%22%20viewBox%3D%220%200%201000%201000%22%20preserveAspectRatio%3D%22xMidYMid%20slice%22%20version%3D%221.1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0A%20%20%3Cdefs%3E%0A%20%20%20%20%3CradialGradient%20id%3D%22rg0%22%20cx%3D%220.218%22%20cy%3D%220.78%22%20r%3D%221%22%3E%0A%20%20%20%20%20%20%3Cstop%20offset%3D%220%25%22%20stop-color%3D%22%238b5cf61e%22%20stop-opacity%3D%221%22%20%2F%3E%0A%20%20%20%20%20%20%3Cstop%20offset%3D%2250%25%22%20stop-color%3D%22%238b5cf61e%22%20stop-opacity%3D%220%22%20%2F%3E%0A%20%20%20%20%3C%2FradialGradient%3E%0A%20%20%20%20%3Cfilter%20id%3D%22f0%22%3E%0A%20%20%20%20%20%20%3CfeColorMatrix%20type%3D%22hueRotate%22%20values%3D%2227%22%20%2F%3E%0A%20%20%20%20%3C%2Ffilter%3E%0A%20%20%20%20%3CradialGradient%20id%3D%22rg1%22%20cx%3D%220.805%22%20cy%3D%220.499%22%20r%3D%221%22%3E%0A%20%20%20%20%20%20%3Cstop%20offset%3D%220%25%22%20stop-color%3D%22%238b5cf61e%22%20stop-opacity%3D%221%22%20%2F%3E%0A%20%20%20%20%20%20%3Cstop%20offset%3D%2250%25%22%20stop-color%3D%22%238b5cf61e%22%20stop-opacity%3D%220%22%20%2F%3E%0A%20%20%20%20%3C%2FradialGradient%3E%0A%20%20%20%20%3Cfilter%20id%3D%22f1%22%3E%0A%20%20%20%20%20%20%3CfeColorMatrix%20type%3D%22hueRotate%22%20values%3D%222%22%20%2F%3E%0A%20%20%20%20%3C%2Ffilter%3E%0A%20%20%3C%2Fdefs%3E%0A%20%20%3Crect%20fill%3D%22none%22%20x%3D%220%25%22%20y%3D%220%25%22%20width%3D%22100%25%22%20height%3D%22100%25%22%20%2F%3E%0A%20%20%3Crect%20fill%3D%22url(%23rg0)%22%20filter%3D%22url(%23f0)%22%20x%3D%220%25%22%20y%3D%220%25%22%20width%3D%22100%25%22%20height%3D%22100%25%22%20%2F%3E%0A%20%20%3Crect%20fill%3D%22url(%23rg1)%22%20filter%3D%22url(%23f1)%22%20x%3D%220%25%22%20y%3D%220%25%22%20width%3D%22100%25%22%20height%3D%22100%25%22%20%2F%3E%0A%3C%2Fsvg%3E" />
+  )
+
+}
+
+function Divider() {
+  return (
+    <div className="flex items-center gap-2">
+      <div className="bg-slate-400 flex-1 h-px" />
+      <span className="text-sm text-neutral-500">or</span>
+      <div className="bg-slate-400 flex-1 h-px" />
+    </div>
+  );
+}
+
+//
+// Auth components
+// ----------------------
+function GoogleAuth() {
+  const [error, setError] = useState(null);
+  const [nonce] = useState(crypto.randomUUID());
+  return (
+    <GoogleOAuthProvider
+      clientId="416673654793-s0grgnmg2s86h1re3ipaumqq64kloq7o.apps.googleusercontent.com"
+      nonce={nonce}
+    >
+      <div className="flex justify-center items-center mt-4">
+        <GoogleLogin
+          nonce={nonce}
+          onSuccess={(credentialResponse) => {
+            const idToken = credentialResponse.credential;
+            if (!idToken) {
+              setError("Missing id_token.");
+              return;
+            }
+            db.auth.signInWithIdToken({
+              clientName: "instant-awedience",
+              idToken,
+              nonce,
+            }).catch((err) => {
+              console.log(err.body);
+              alert("Uh oh: " + err.body?.message);
+            });
+          }}
+          onError={() => {
+            setError("Login failed.");
+          }}
+          type="standard"
+        />
+        {error}
+      </div>
+    </GoogleOAuthProvider>
+  )
+}
+
+function Login() {
+  const [sentEmail, setSentEmail] = useState('')
+  return (
+    <div className="h-screen p-4 sm:p-20 flex-1 justify-center items-center flex">
+      {!sentEmail ? (
+        <Email setSentEmail={setSentEmail} />
+      ) : (
+        <MagicCode sentEmail={sentEmail} />
+      )}
+    </div>
+  )
+}
+
+function Email({ setSentEmail }) {
+  const [email, setEmail] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!email) return
+    setSentEmail(email)
+    db.auth.sendMagicCode({ email }).catch((err) => {
+      alert('Uh oh :' + err.body?.message)
+      setSentEmail('')
+    })
+  }
+  return (
+    <div className={authContainerStyle}>
+      <h5 className={authHeaderStyle}>Sign in with Email</h5>
+      <p className={authSubheaderStyle}>We'll send you an email with a special link that you can use to sign in, no password needed!</p>
+      <form onSubmit={handleSubmit} className="mb-4">
+        <input
+          className={inputStyle}
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="example@email.com" />
+        <AuthButton label="Send link" />
+      </form >
+      <Divider />
+      <GoogleAuth />
+      <Background />
+    </div>
+  );
+}
+function MagicCode({ sentEmail }) {
+  const [code, setCode] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    db.auth.signInWithMagicCode({ email: sentEmail, code }).catch((err) => {
+      alert('Uh oh :' + err.body?.message)
+      setCode('')
+    })
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={authContainerStyle}>
+        <h5 className={authHeaderStyle}>Enter your code</h5>
+        <p className={authSubheaderStyle}>We'll send you an email with a special link that you can use to sign in, no password needed!</p>
+        <input
+          className={inputStyle}
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+          placeholder="12345..." />
+        <AuthButton label="Verify" />
+      </div>
+      <Background />
+    </form >
+  );
+}
+
+export default Login;
---
+++ src/components/partytime.js
@@ -0,0 +1,116 @@
+import { init } from '@instantdb/react';
+import { createRef, useRef } from 'react';
+
+const { usePublishTopic, useTopicEffect } = init({
+  appId: "d7e379b9-9744-4ba1-a7d4-4b022080338d",
+});
+
+export default function InstantTopics() {
+  const sendEmoji = usePublishTopic('topics-example', '1', 'emoji');
+
+  useTopicEffect(
+    'topics-example',
+    '1',
+    'emoji',
+    ({ name, directionAngle, rotationAngle }) => {
+      if (!emoji[name]) return;
+      emote(
+        { emoji: emoji[name], directionAngle, rotationAngle },
+        elRefsRef.current[name].current
+      );
+    }
+  );
+
+  const elRefsRef = useRef(refsInit);
+
+  return (
+    <div className="flex gap-4">
+      {emojiNames.map((name) => (
+        <div className="relative" key={name} ref={elRefsRef.current[name]}>
+          <button
+            className={emojiClassNames}
+            onClick={() => {
+              const params = {
+                name,
+                rotationAngle: 360 * Math.random(),
+                directionAngle: 135 + Math.random() * 90,
+              };
+              emote(
+                {
+                  emoji: emoji[name],
+                  rotationAngle: params.rotationAngle,
+                  directionAngle: params.directionAngle,
+                },
+                elRefsRef.current[name].current
+              );
+
+              sendEmoji(params);
+            }}
+          >
+            {emoji[name]}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function emote(
+  config,
+  target
+) {
+  if (!target) return;
+  const d1 = document.createElement('div');
+  const d2 = document.createElement('div');
+  const d3 = document.createElement('div');
+
+  target.appendChild(d1);
+  d1.appendChild(d2);
+  d2.appendChild(d3);
+  d3.innerText = config.emoji;
+
+  requestAnimationFrame(() => {
+    setTimeout(() => {
+      Object.assign(d2.style, {
+        transform: `translateY(80vh) scale(2)`,
+        transition: 'all 400ms',
+        opacity: '0',
+      });
+    });
+  });
+
+  setTimeout(() => d1.remove(), 800);
+
+  Object.assign(d1.style, {
+    transform: `rotate(${config.directionAngle}deg)`,
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    margin: 'auto',
+    zIndex: '9999',
+    pointerEvents: 'none',
+  });
+
+  Object.assign(d3.style, {
+    transform: `rotateZ(${config.rotationAngle}deg)`,
+    fontSize: `40px`,
+  });
+}
+
+const emoji = {
+  fire: '🔥',
+  wave: '👋',
+  confetti: '🎉',
+  heart: '❤️',
+};
+
+const emojiNames = Object.keys(emoji);
+
+const refsInit = Object.fromEntries(
+  emojiNames.map((name) => [name, createRef()])
+);
+
+const emojiClassNames =
+  'rounded-lg bg-white p-3 text-3xl shadow-md transition duration-200 ease-in-out hover:-translate-y-1 hover:shadow-xl';
